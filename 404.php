831390263c3350c701fa30473912630513f9d705
Camila Stefano <cstefano@makingsense.com.ar>
2022-07-13 17:26:06 -0300

    feat(404): add 404 page

---
+++ 404.php
@@ -0,0 +1,56 @@
+<?php
+    define('VERSION', '1.0.0')
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EMMS 2022: 404</title>
+
+  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="../css/header.css">
+</head>
+
+<body>
+
+  <main class="emms22__home">
+
+    <!-- Header -->
+    <nav class="emms22__header">
+      <div class="emms22__container--lg">
+        <div class="emms22__header__logo">
+          <a href="index.php#inicio"><img src="../img/logo-emms.png?version=<?= VERSION ?>" alt="Emms 2022"
+              id="logo-emms-404"></a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- main -->
+    <section class="emms22__404">
+      <div class="emms22__container--lg ">
+        <div class="emms22__404__container">
+          <div class="error-404">
+          </div>
+
+          <div class="emms22__404__error error-404-message">
+            <p>¿Te has perdido? Lo que no puedes perderte es el evento exclusivo que estamos preparando para ti.
+            </p>
+            <button type="button" class="emms22__404__button" id="error-404-btn"
+              onclick="window.location.href='/index.php'">
+              VOLVER A EMMS2022
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+  </main>
+
+</body>
+
+</html>
