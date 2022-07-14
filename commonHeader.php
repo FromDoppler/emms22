b65356edf905d83ad357dabb5758fc426d767715
Matías Roy Rodriguez <mroy@makingsense.com>
2022-07-14 18:38:06 -0300

    fix: change header year

--- commonHeader.php
+++ commonHeader.php
@@ -1,4 +1,4 @@
-	<title>EMMS 2021: Conferencias online de Marketing Digital.</title>
+	<title>EMMS 2022: Conferencias online de Marketing Digital.</title>
 	<meta name="robots" content="<?= (PRODUCTION) ? 'index, follow' : 'noindex, nofollow' ?>">
 
 	<meta name="theme-color" content="#3F0453">
