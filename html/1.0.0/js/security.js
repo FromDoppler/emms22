4766c86f98b2f11d69ce9148455ba28a559cac38
MS\hcardoso <hernan.f.cardoso@gmail.com>
2022-07-14 16:58:51 -0300

    fix parameters utms after submit

--- html/1.0.0/js/security.js
+++ html/1.0.0/js/security.js
@@ -1,9 +1,15 @@
 const getLoged = localStorage.getItem('registered');
+const utm_source = (document.getElementById("utm_source").value != '' ? document.getElementById("utm_source").value : '');
+const utm_campaign = (document.getElementById("utm_campaign").value != '' ? document.getElementById("utm_campaign").value : '');
+const utm_content = (document.getElementById("utm_content").value != '' ? document.getElementById("utm_content").value : '');
+const utm_term = (document.getElementById("utm_term").value != '' ? document.getElementById("utm_term").value : '');
+const utm_medium = (document.getElementById("utm_medium").value != '' ? document.getElementById("utm_medium").value : '');
 
 function addParameterUTms() {
 
 	const urlParamsValues = [utm_medium, utm_source, utm_campaign, utm_term, utm_content]
 	const urlParamsNames = ['utm_medium', 'utm_source', 'utm_campaign', 'utm_term', 'utm_content'];
+
 	let newParamsUrl = '?';
 	let firstParam = true;
 	urlParamsValues.forEach((param, index) => {
