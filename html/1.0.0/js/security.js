e14693c695284d068160790fb8bc8abe731b23e0
Matías Roy Rodriguez <mroy@makingsense.com>
2022-07-14 10:34:24 -0300

    chore: commit in progress function

--- html/1.0.0/js/security.js
+++ html/1.0.0/js/security.js
@@ -4,12 +4,12 @@ if (getLoged === null) {
 	console.log('no')
 	window.location.href = '/index.php';
 }
-const checkUserLoged = function checkUserLoged() {
+//const checkUserLoged = function checkUserLoged() {
 
-	if (getLoged != null) {
-		window.location.href = '/secondIndex.php';
-	}
+	//if (getLoged != null) {
+		//window.location.href = '/secondIndex.php';
+	//}
 
-}
+//}
 
-export { checkUserLoged };
+//export { checkUserLoged };
