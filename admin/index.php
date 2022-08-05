b2a1e866afebbdec845335a8bf10cee2baf3decf
MS\hcardoso <hernan.f.cardoso@gmail.com>
2022-08-05 09:43:49 -0300

    add aliados starter - falta eliminar el campo link

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ABMs EMMS</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+
+</script>
+</head>
+<body>
+<center>
+
+<div id="container"> <div id="table-responsive">
+    <h2>Listados Entidades</h2>
+    </div>
+</div>
+    <br/>
+    <br/>
+    <a href="aliados_pro/index.php"> ABM Listado Aliados Pro</a>
+    <br/>
+    <br/>
+     <a href="aliados_starter/index.php"> ABM Listado Aliados Starter</a>
+
+<div id="container">
+
+
+
+
+</div>
+</body>
+</html>
