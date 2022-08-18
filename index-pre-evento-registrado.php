dde1aca62b3ed014189e8dc3023e7c5d1cab45c1
ornella-688 <ornella.cicalello@gmail.com>
2022-08-18 10:05:35 -0300

    fix(pre-event): content speakers and fixes

--- index-pre-evento-registrado.php
+++ index-pre-evento-registrado.php
@@ -725,7 +725,7 @@ require_once('config.php');
 							<li>Cursas a tu ritmo</li>
 							<li>Clases teóricas y prácticas</li>
 						</ul>
-						<button class="emms22__button emms22__fade-in">INSCRÍBETE AHORA</button>
+						<a href="https://academy.fromdoppler.com/bootcamp/" target="_blank" class="emms22__button emms22__fade-in">INSCRÍBETE AHORA</a>
 					</div>
 				</div>
 			</div>
