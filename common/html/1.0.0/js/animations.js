544a7576201960f6f1daa55e2b3e113bfc4a4b6f
ornella-688 <ornella.cicalello@gmail.com>
2022-09-16 17:39:24 -0300

    feat(calendar): speaker bio added

--- common/html/1.0.0/js/animations.js
+++ common/html/1.0.0/js/animations.js
@@ -184,6 +184,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	}
 
+
+    // Show/Hide Biography Speaker
+    const btnsBio = document.querySelectorAll(".emms22__pre-event__calendar__event-list__event__card__btn-bio");
+    const btnBioHide = document.querySelectorAll(".emms22__pre-event__calendar__event-list__event__card__btn-bio-hide");
+    const classBioShow = "emms22__pre-event__calendar__event-list__event__card__bio--show";
+
+    btnsBio.forEach(btn => {
+        btn.addEventListener("click", () => {
+            hideAllBios();
+            btn.parentNode.querySelector(".bio-speaker").classList.toggle(classBioShow);
+        });
+    })
+
+    btnBioHide.forEach(btnHide =>{
+        btnHide.addEventListener("click", () => {
+            btnHide.parentNode.classList.toggle(classBioShow);
+        });
+    })
+
+    function hideAllBios(){
+        document.querySelectorAll(".bio-speaker").forEach ( bio => {
+            bio.classList.remove(classBioShow);
+        })
+
+
+    }
+
 });
 
 
